25f82fbbfbed97c8740a429a4e079b35bc79bcd0
Meze <mezequielabogado@gmail.com>
2023-05-11 21:13:09 -0300

    Feat: se agrega primer approach al toggle de theme para el registro page

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,39 @@
-function changeTheme(isDarkTheme) {
-    if (isDarkTheme) {
-        alert("HOLA")
-        document.body.classList.add('light-theme');
-        document.body.classList.remove('dark-theme');
+function changeTheme() {
+    const themeButton = document.getElementById("themeButton");
+    const isDark = (themeButton.getAttribute("value") === "true");
+
+    if (isDark) {
+        // Cambiar a tema claro
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+        themeButton.setAttribute("value", "false");
+
+        const signUp = document.getElementById("sign-up");
+        signUp.classList.remove("dark-theme");
+        signUp.classList.add("light-theme");
+        signUp.style.backgroundColor = "white";
+        signUp.style.color = "black";
+
+        const signUpForm = document.getElementById("sign-up-form");
+        signUpForm.classList.remove("dark-theme");
+        signUpForm.classList.add("light-theme");
+        signUpForm.style.backgroundColor = "white";
     } else {
-        document.body.classList.add('dark-theme');
-        document.body.classList.remove('light-theme');
+        // Cambiar a tema oscuro
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+        themeButton.setAttribute("value", "true");
+
+        const signUp = document.getElementById("sign-up");
+        signUp.classList.remove("light-theme");
+        signUp.classList.add("dark-theme");
+        signUp.style.backgroundColor = "";
+        signUp.style.color = "";
+
+        const signUpForm = document.getElementById("sign-up-form");
+        signUpForm.classList.remove("light-theme");
+        signUpForm.classList.add("dark-theme");
+        signUpForm.style.backgroundColor = "";
     }
 }
+
